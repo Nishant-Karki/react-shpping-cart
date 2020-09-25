dbaa178c2257b4fe2f96f4667662f5d8784d8a25
Nishant <karkinishant14@gmail.com>
2020-09-25 17:41:02 +0545

    first commit

---
+++ src/components/FooterCom.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FooterCom() {
+  return (
+    <footer className="page-footer bg-dark">
+      <div className="text-center text-light p-1">
+        <p>All right is reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default FooterCom;
---
+++ src/components/HeaderCom.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeaderCom() {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          React Shopping Cart
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default HeaderCom;
